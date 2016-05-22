d9f7486fd33df0b88c05b600d39f839da65c140f
Ibemu <aa2a3a4a5a6a7a8a9@yahoo.co.jp>
2016-05-22 20:46:50 +0900

    Quest: 消滅したクエストの一部を自動的に削除する

--- src/ibemu/logbook/plugin/quest/api/ApiGetMemberQuestlist.java
+++ src/ibemu/logbook/plugin/quest/api/ApiGetMemberQuestlist.java
@@ -1,5 +1,8 @@
 package ibemu.logbook.plugin.quest.api;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonValue;
@@ -24,6 +27,9 @@ public class ApiGetMemberQuestlist implements APIListenerSpi
         JsonObject apidata = json.getJsonObject("api_data");
         if (!apidata.isNull("api_list")) {
             JsonArray apilist = apidata.getJsonArray("api_list");
+            Set<Integer> keys = new HashSet<Integer>();
+            int min = 9999;
+            int max = 0;
             for (JsonValue value : apilist) {
                 if (value instanceof JsonObject) {
                     JsonObject questobject = (JsonObject) value;
@@ -48,8 +54,15 @@ public class ApiGetMemberQuestlist implements APIListenerSpi
                         break;
                     }
                     QuestCollection.get().getQuestMap().put(key, quest);
+                    if(key < min) min = key;
+                    if(max < key) max = key;
+                    keys.add(key);
                 }
             }
+            // TODO: マシな方法
+            int min2 = min;
+            int max2 = max;
+            QuestCollection.get().getQuestMap().entrySet().removeIf(e -> (min2 < e.getKey()) && (e.getKey() < max2) && (!keys.contains(e.getKey())));
             ThreadManager.getExecutorService().execute(Config.getDefault()::store);
         }
     }
