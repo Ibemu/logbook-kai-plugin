bda96f00adb2446810855ef34c9ce1104fcb45c1
Ibemu <aa2a3a4a5a6a7a8a9@yahoo.co.jp>
2016-06-11 01:17:42 +0900

    Quest: 20160610 アップデート対応

--- src/ibemu/logbook/plugin/quest/api/ApiGetMemberQuestlist.java
+++ src/ibemu/logbook/plugin/quest/api/ApiGetMemberQuestlist.java
@@ -1,13 +1,22 @@
 package ibemu.logbook.plugin.quest.api;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.function.Predicate;
 
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonValue;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import ibemu.logbook.plugin.Config;
+import ibemu.logbook.plugin.quest.ApiHelper;
 import ibemu.logbook.plugin.quest.Quest;
 import ibemu.logbook.plugin.quest.QuestCollection;
 import ibemu.logbook.plugin.quest.QuestDue;
@@ -30,40 +39,103 @@ public class ApiGetMemberQuestlist implements APIListenerSpi
             Set<Integer> keys = new HashSet<Integer>();
             int min = 9999;
             int max = 0;
-            for (JsonValue value : apilist) {
-                if (value instanceof JsonObject) {
-                    JsonObject questobject = (JsonObject) value;
-                    int key = questobject.getInt("api_no");
-                    // 任務を作成
-                    Quest quest = Quest.setQuestlist(QuestCollection.get().getQuestMap().get(key), questobject);
+            int pagecount = apidata.getInt("api_page_count");
+            int disppage = apidata.getInt("api_disp_page");
+            if(pagecount > 0)
+            {
+                if(disppage == 1) min = 0;
+                if(disppage == pagecount) max = 9999;
+                for (JsonValue value : apilist) {
+                    if (value instanceof JsonObject) {
+                        JsonObject questobject = (JsonObject) value;
+                        int key = questobject.getInt("api_no");
+                        // 任務を作成
+                        Quest quest = Quest.setQuestlist(QuestCollection.get().getQuestMap().get(key), questobject);
 
-                    switch (quest.getType())
-                    {
-                    case 2:
-                    case 4:
-                    case 5:
-                        quest.setDue(QuestDue.getDaily());
-                        break;
-                    case 3:
-                        quest.setDue(QuestDue.getWeekly());
-                        break;
-                    case 6:
-                        quest.setDue(QuestDue.getMonthly());
-                        break;
-                    default:
-                        break;
+                        switch (quest.getType())
+                        {
+                        case 1:
+                        case 5:
+                            quest.setDue(QuestDue.getDaily());
+                            break;
+                        case 2:
+                            quest.setDue(QuestDue.getWeekly());
+                            break;
+                        case 3:
+                            quest.setDue(QuestDue.getMonthly());
+                            break;
+                        default:
+                            break;
+                        }
+                        QuestCollection.get().getQuestMap().put(key, quest);
+                        if(key < min) min = key;
+                        if(max < key) max = key;
+                        keys.add(key);
                     }
-                    QuestCollection.get().getQuestMap().put(key, quest);
-                    if(key < min) min = key;
-                    if(max < key) max = key;
-                    keys.add(key);
                 }
             }
-            // TODO: マシな方法
-            int min2 = min;
-            int max2 = max;
-            QuestCollection.get().getQuestMap().entrySet().removeIf(e -> (min2 < e.getKey()) && (e.getKey() < max2) && (!keys.contains(e.getKey())));
+            else
+            {
+                min = 0;
+                max = 9999;
+            }
+            if(req.getRequestBody().isPresent())
+            {
+                try
+                {
+                    Map<String, String> m = ApiHelper.getQueryMap(req.getRequestBody().get());
+                    QuestCollection.get().getQuestMap().entrySet().removeIf(new QuestRemover(min, max, Integer.parseInt(m.get("api_tab_id")), keys));
+                }
+                catch (IOException e)
+                {
+                    LoggerHolder.LOG.warn("任務削除に例外が発生しました", e);
+                }
+            }
             ThreadManager.getExecutorService().execute(Config.getDefault()::store);
         }
+    }
+
+    private class QuestRemover implements Predicate<Entry<Integer, Quest>>
+    {
+        private final int min;
+        private final int max;
+        private final int tab;
+        private final Set<Integer> keys;
+
+        public QuestRemover(int min, int max, int tab, Set<Integer> keys)
+        {
+            this.min = min;
+            this.max = max;
+            this.tab = tab;
+            this.keys = keys;
+
+        }
+
+        @Override
+        public boolean test(Entry<Integer, Quest> e)
+        {
+            boolean base = (min < e.getKey()) && (e.getKey() < max) && (!keys.contains(e.getKey()));
+            int state = e.getValue().getState();
+            int type = e.getValue().getType();
+            switch(tab)
+            {
+            case 0:
+                //全 All
+                return base;
+
+            case 9:
+                //遂行中任務
+                return base && (state == 2 || state == 3);
+
+            default:
+                return base && type == tab;
+            }
+        }
+
+    }
+
+    private static class LoggerHolder {
+        /** ロガー */
+        private static final Logger LOG = LogManager.getLogger(ApiReqQuestClearitemget.class);
     }
 }
